c49449b041c6f684e5ff30a3d705bb2ce117f725
Nico Bellanich <nicolasbellanich@gmail.com>
2022-10-12 15:39:08 -0300

    add maps

--- src/App.js
+++ src/App.js
@@ -1,43 +1,69 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 function App() {
+  const [stNumberToAskFor, setStNumberToAskFor] = useState("");
+  const [stNameToAskFor, setStNameToAskFor] = useState("");
 
-  const [latitudeToAskFor,setLatitudeToAskFor] = useState("");
-  const [longitudeToAskFor,setLongitudeToAskFor] = useState("");
-
-
-  const handleLatitudeInputChange = (e) => {
-    setLatitudeToAskFor(e.target.value);
+  const handlestNumberInputChange = (e) => {
+    setStNumberToAskFor(e.target.value);
   };
 
-  const handleLongitudeInputChange = (e) => {
-    setLongitudeToAskFor(e.target.value);
+  const handlestNameInputChange = (e) => {
+    setStNameToAskFor(e.target.value);
   };
-
 
   const searchCloserFoodTruck = async () => {
-    console.log(latitudeToAskFor,longitudeToAskFor)
-    const response = await axios 
-      .get(`http://127.0.0.1:8000/food_trucks/${latitudeToAskFor}/${longitudeToAskFor}`)
+    console.log(stNumberToAskFor, stNameToAskFor);
+    const response = await axios
+      .get(
+        `http://127.0.0.1:8000/food_trucks/${stNumberToAskFor}/${stNameToAskFor}`
+      )
       .then((r) => {
         console.log(r);
       })
       .catch((e) => console.log(e));
-  }
+  };
 
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD9NcAPaKmfcBbmdvulwtvNs1-0k-Ds8mA",
+  });
+
+  if (!isLoaded) return <div> LOADING </div>;
   return (
     <div className="App">
       <div className="App-header">
         <h1 className="App-title">Welcome to food trucks app</h1>
-        <p className="App-p"> Please provide latitude and longitude to search distance</p>
-        <input placeholder="latitude" onChange={handleLatitudeInputChange}></input>
-        <input placeholder="longitude" onChange={handleLongitudeInputChange}></input>
-        <button onClick={() => searchCloserFoodTruck()}> Look for the closer food truck </button>
+        <p className="App-p">
+          Please provide the street number and street name to search closer food truck
+        </p>
+        <div className="App-container-inputs">
+          <input
+            className="App-inputs"
+            placeholder="Street Number"
+            onChange={handlestNumberInputChange}
+          ></input>
+          <input
+            className="App-inputs"
+            placeholder="Street Name"
+            onChange={handlestNameInputChange}
+          ></input>
+        </div>
+        <button onClick={() => searchCloserFoodTruck()}>
+          Look for the closer food truck{" "}
+        </button>
       </div>
       <div>
         <h1>Map</h1>
+        <GoogleMap
+          zoom={10}
+          center={{ lat: 44, lng: -80 }}
+          mapContainerClassName="map-container"
+        >
+          <Marker position={{ lat: 44, lng: -80 }} />
+        </GoogleMap>
       </div>
     </div>
   );
