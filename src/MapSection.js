29bc1a10b0304108036706c9888b3a03f0739b87
Nico Bellanich <nicolasbellanich@gmail.com>
2022-10-13 00:35:17 -0300

    find directions on maps

--- src/MapSection.js
+++ src/MapSection.js
@@ -5,7 +5,12 @@ import useWindowDimensions from "./useWindowDimension.js";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
-export function MapSection({ initLat, finishLat, initLon, finishLon }) {
+export function MapSection({
+  initialLatitude,
+  finalLatitude,
+  initialLongitude,
+  finalLongitude,
+}) {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyD9NcAPaKmfcBbmdvulwtvNs1-0k-Ds8mA",
   });
@@ -28,20 +33,22 @@ export function MapSection({ initLat, finishLat, initLon, finishLon }) {
           <h1>{"Starting Point"}</h1>
           <GoogleMap
             zoom={17}
-            center={{ lat: initLat, lng: initLon }}
+            center={{ lat: initialLatitude, lng: initialLongitude }}
             mapContainerClassName="Single-map"
           >
-            <Marker position={{ lat: initLat, lng: initLon }} />
+            <Marker
+              position={{ lat: initialLatitude, lng: initialLongitude }}
+            />
           </GoogleMap>
         </Grid>
         <Grid item xs={!isDesktopSize ? 12 : 6}>
           <h1>{"Finishing Point"}</h1>
           <GoogleMap
             zoom={17}
-            center={{ lat: finishLat, lng: finishLon }}
+            center={{ lat: finalLatitude, lng: finalLongitude }}
             mapContainerClassName="Single-map"
           >
-            <Marker position={{ lat: finishLat, lng: finishLon }} />
+            <Marker position={{ lat: finalLatitude, lng: finalLongitude }} />
           </GoogleMap>
         </Grid>
       </Grid>
